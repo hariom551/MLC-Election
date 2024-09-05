4fd7c310aee0e81871a7395a044255bc82190991
hariom nathani <hariomnathani551@gmail.com>
2024-09-05 12:50:17 +0530

    home

--- client/src/components/Pages/Home.jsx
+++ client/src/components/Pages/Home.jsx
@@ -10,6 +10,11 @@ function Home() {
                 <h1 className='w-1000 flex justify-center items-center text-black mb-3 text-xl mt-10'>
                     {role === 'Super Admin' && "Welcome, Super Admin!"}
                     {role === 'Admin' && "Welcome, Admin!"}
+                    {role === 'Forms Admin' && "Welcome, Forms Admin!"}
+                    {role === 'Sub Admin' && "Welcome, Sub Admin!"}
+                    {role === 'QC Staff' && "Welcome, QC Staff!"}
+                    {role === 'Feeding Staff' && "Welcome, Feeding Staff!"}
+
                     {/* Add more role-specific content here as needed */}
                 </h1>
             </div>
